02bd6576ae1d89d03cee54cb7a12b14f6de2b107
Hugo Sosa <hugososa26.hs@gmail.com>
2025-11-07 01:12:25 -0300

    Actualiza estilos, HTML, elimina imágenes antiguas y agrega nuevos archivos

---
+++ JAVASCRIPT/validaciones.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("formContacto");
+
+    form.addEventListener("submit", function(event)
+    {
+        event.preventDefault();
+
+        const nombre = form.nombre.value.trim();
+        const email = form.email.value.trim();
+        const asunto = form.asunto.value.trim();
+        const motivo = form.motivo.value;
+        const mensaje = form.mensaje.value.trim();
+        const tipo = form.querySelector("input[name='tipo']:checked");
+
+        if(!nombre)
+        {
+            alert("Por favor ingrese un nombre válido.");
+            return;
+        }
+
+        if(!email || !email.includes("@"))
+        {
+            alert("Por favor ingrese una dirección de correo válida.");
+            return;
+        }
+
+        if(!asunto)
+        {
+            alert("Debe ingresar un asunto para el mensaje.");
+            return;
+        }
+
+        if(!motivo)
+        {
+            alert("Por favor seleccione un motivo de contacto.");
+            return;
+        }
+
+        if(!mensaje)
+        {
+            alert("Por favor escriba su mensaje para completar el formulario. Gracias");
+            return;
+        }
+
+        alert("¡Mensaje enviado correctamente! Gracias por contactarnos.");
+        form.reset();
+
+    });
